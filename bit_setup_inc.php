21d5da9faa74c7e7bd4114b7aa4987d8dfee4f12
bitweaver.org <bitweaver@users.sourceforge.net>
2006-01-31 21:24:58 +0000

    more tidbits separation

---
+++ bit_setup_inc.php
@@ -0,0 +1,18 @@
+<?php
+	$gBitSystem->registerPackage( 'tidbits', dirname( __FILE__).'/', FALSE );
+
+
+	if( $gBitUser->isRegistered() && $gBitSystem->getPreference('feature_usermenu') == 'y' ) {
+		if (!isset($_SESSION['usermenu'])) {
+			include_once(TIDBITS_PKG_PATH . 'user_menu_lib.php');
+
+			$user_menus = $usermenulib->list_usermenus($gBitUser->mUserId, 0, -1, 'position_asc', '');
+			$gBitSmarty->assign('usr_user_menus', $user_menus['data']);
+			$_SESSION['usermenu'] = $user_menus['data'];
+		} else {
+			$user_menus = $_SESSION['usermenu'];
+			$gBitSmarty->assign('usr_user_menus', $user_menus);
+		}
+	}
+
+?>
