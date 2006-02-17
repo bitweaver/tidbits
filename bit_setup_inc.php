51b4904079f4899542748523de48c68449b100bd
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:54 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,19 +1,23 @@
 <?php
-	$gBitSystem->registerPackage( 'tidbits', dirname( __FILE__).'/');
+global $gBitSystem;
+$registerHash = array(
+	'package_name' => 'tidbits',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
+if( $gBitUser->isRegistered() && $gBitSystem->getPreference('usermenu') == 'y' ) {
 
-	if( $gBitUser->isRegistered() && $gBitSystem->getPreference('usermenu') == 'y' ) {
+	if (!isset($_SESSION['usermenu'])) {
+		include_once(TIDBITS_PKG_PATH . 'user_menu_lib.php');
 
-		if (!isset($_SESSION['usermenu'])) {
-			include_once(TIDBITS_PKG_PATH . 'user_menu_lib.php');
-
-			$user_menus = $usermenulib->list_usermenus($gBitUser->mUserId, 0, -1, 'position_asc', '');
-			$gBitSmarty->assign('usr_user_menus', $user_menus['data']);
-			$_SESSION['usermenu'] = $user_menus['data'];
-		} else {
-			$user_menus = $_SESSION['usermenu'];
-			$gBitSmarty->assign('usr_user_menus', $user_menus);
-		}
+		$user_menus = $usermenulib->list_usermenus($gBitUser->mUserId, 0, -1, 'position_asc', '');
+		$gBitSmarty->assign('usr_user_menus', $user_menus['data']);
+		$_SESSION['usermenu'] = $user_menus['data'];
+	} else {
+		$user_menus = $_SESSION['usermenu'];
+		$gBitSmarty->assign('usr_user_menus', $user_menus);
 	}
+}
 
 ?>
