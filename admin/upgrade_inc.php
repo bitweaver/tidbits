a52fe40d4e45c709735946e7fe7384359bd20a3c
Max Kremmel <xing@synapse.plus.com>
2006-03-20 08:12:55 +0000

    upgrader brought up to date with 07-FEB-2006 on SchemaChangeLog - untested

--- admin/upgrade_inc.php
+++ admin/upgrade_inc.php
@@ -5,19 +5,6 @@ $upgrades = array(
 
 	'BWR1' => array(
 		'BWR2' => array(
-// de-tikify tables
-array( 'DATADICT' => array(
-	array( 'RENAMETABLE' => array(
-		'tiki_user_bookmarks_urls'    => 'tidbits_user_bookmarks_urls',
-		'tiki_user_menus'             => 'tidbits_user_menus',
-		'tiki_user_tasks'             => 'tidbits_user_tasks',
-		'tiki_user_bookmarks_folders' => 'tidbits_user_bookmarks_folders',
-		'tiki_user_postings'          => 'tidbits_user_postings',
-		'tiki_user_votings'           => 'tidbits_user_votings',
-		'tiki_userpoints'             => 'tidbits_userpoints',
-		'tiki_userfiles'              => 'tidbits_userfiles',
-	)),
-)),
 		)
 	),
 
