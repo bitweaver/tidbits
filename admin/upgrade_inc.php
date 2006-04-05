feb6681a4df7a664607ebf2e5c3e60354a266d6a
Max Kremmel <xing@synapse.plus.com>
2006-04-05 13:30:37 +0000

    tonnes of upgrader error sqashes - liberty_content_links still remain to be dealt with

--- admin/upgrade_inc.php
+++ admin/upgrade_inc.php
@@ -6,16 +6,29 @@ $upgrades = array(
 	'BWR1' => array(
 		'BWR2' => array(
 array( 'DATADICT' => array(
+	array( 'RENAMETABLE' => array(
+		'tiki_user_menus'             => 'tidbits_menus',
+		'tiki_user_tasks'             => 'tidbits_tasks',
+		'tiki_user_bookmarks_folders' => 'tidbits_bookmarks_folders',
+		'tiki_user_bookmarks_urls'    => 'tidbits_bookmarks_urls',
+		'tiki_user_postings'          => 'tidbits_postings',
+		'tiki_user_votings'           => 'tidbits_votings',
+		'tiki_banning'                => 'tidbits_banning',
+		'tiki_banning_sections'       => 'tidbits_banning_sections',
+		'tiki_cookies'                => 'tidbits_fortune_cookies',
+		'tiki_userpoints'             => 'tidbits_userpoints',
+		'tiki_userfiles'              => 'tidbits_userfiles',
+	)),
 	array( 'RENAMECOLUMN' => array(
 		'tidbits_menus' => array(
-			'`position`' => 'menu_position',
+			'`position`' => '`menu_position` I4',
 		),
 		'tidbits_tasks' => array(
-			'`date`' => 'task_date',
+			'`date`' => '`task_date` I8',
 		),
 		'tidbits_banning' => array(
-			'`user`' => 'ban_user',
-			'`message`' => 'ban_message',
+			'`user`' => '`ban_user` C(40)',
+			'`message`' => '`ban_message` X',
 		),
 	)),
 )),
